0e8016fefa778425209e2b5aed6e9e15119c6369
somanshtea11 <somanshtea@gmail.com>
2026-02-12 16:16:43 +0530

    Add Footer component

---
+++ frontend/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <h4>Footer Content Here</h4>
+                <p>Current Date and Time: 2026-02-12 10:46:03</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
